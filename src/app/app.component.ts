64644cef39d60ef8d49fd7a1240d5c49b4dbdb4c
Wagner Colombo Dias <wagco@outlook.com>
2021-09-05 22:21:08 -0300

    Applying alternative css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+  h1{
+    color: orange;
+    }`]
 })
 export class AppComponent {
   title = 'my-first-app';
