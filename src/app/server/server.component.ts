5dcfce0717b6a76bf60e2d2bdd653e9fcc8da8d2
Wagner Colombo Dias <wagco@outlook.com>
2021-09-05 21:45:48 -0300

    Rename files

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './app.server.component.html'
+    templateUrl: './server.component.html'
 })
 export class ServerComponent{
 
